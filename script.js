8923caca1743737e4966990367c3447c1c36d190
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-16 17:01:13 +0000

    Switch to css variables part 1

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+console.log('started');
 const box = document.getElementById('box');
-const globalInput = document.getElementById('global-input');
+inputElements = document.querySelectorAll('input');
+console.log(inputElements);
+for (let i = 0; i < inputElements.length; i++) {
+  console.log('added');
+  inputElements[i].addEventListener('input', inputChanged);
+}
 
-box.style.borderRadius = '40px';
-
-globalInput.addEventListener('input', () => {
-  box.style.borderRadius = globalInput.value;
-})
+function inputChanged(event) {
+  document.documentElement.style.setProperty(
+    `--${event.target.name}`,
+    event.target.value
+  )
+}
